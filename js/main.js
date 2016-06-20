0c503d71abdc58e08335daeac8e84f964a03c3e6
Kelly Rhodes <krhodes1@fullsail.edu>
2016-06-20 11:29:58 -0400

    drawing lines, calc dist

--- js/main.js
+++ js/main.js
@@ -13,20 +13,10 @@ function initialize() {
     mapOptions);
 
   setMarkers(map);
+  setNetwork(map);
 
   // create markers for points of presence (pops)
-  function setMarkers(map) {
-    var pops = [
-      ['New York', 40.741355, -74.003203],
-      ['Ashburn', 39.016363, -77.459022],
-      ['Atlanta', 33.755456, -84.39153],
-      ['Dallas', 32.799852, -96.820433],
-      ['Los Angeles', 34.047908, -118.255536],
-      ['Chicago', 41.854159, -87.619014],
-      ['Seattle', 47.6143, -122.3388],
-      ['Miami', 25.782648, -80.193157],
-    ];
-
+  function setMarkers(map){
     var shape = {
       coords: [1, 1, 1, 20, 18, 20, 18, 1],
       type: 'poly'
@@ -41,9 +31,76 @@ function initialize() {
         title: pop[0],
       });
     };  
-  };  
+  };
 
-}
+  // create "network" connections
+  function setNetwork(map){
+    var networkCoordinates1 = [
+      {lat: pops[0][1], lng: pops[0][2]}, // New York
+      {lat: pops[1][1], lng: pops[1][2]}, // DC
+      {lat: pops[2][1], lng: pops[2][2]}, // Atlanta
+      {lat: pops[7][1], lng: pops[7][2]}, // Miami
+      {lat: pops[3][1], lng: pops[3][2]}, // Dallas
+      {lat: pops[4][1], lng: pops[4][2]}, // LAX
+      {lat: pops[6][1], lng: pops[6][2]}, // Seattle
+      {lat: pops[5][1], lng: pops[5][2]}, // Chicago
+      {lat: pops[0][1], lng: pops[0][2]}, // New York
+    ];
+
+    var networkPath1 = new google.maps.Polyline({
+      path: networkCoordinates1,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    var networkCoordinates2 = [
+      {lat: pops[2][1], lng: pops[2][2]}, // Atlanta
+      {lat: pops[3][1], lng: pops[3][2]}, // Dallas
+    ];
+
+    var networkPath2 = new google.maps.Polyline({
+      path: networkCoordinates2,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+  networkPath1.setMap(map);
+  networkPath2.setMap(map);
+  };
+
+  // calculate distances
+  console.log(pops[0][0], ' to ', pops[1][0]);
+  console.log(distance(pops[0][1], pops[0][2], pops[1][1], pops[1][1])); // NY to DC
+  
+  console.log(pops[1][0], ' to ', pops[2][0]);
+  console.log(distance(pops[1][1], pops[1][2], pops[2][1], pops[2][1])); // DC to Atl
+  
+  console.log(pops[2][0], ' to ', pops[7][0]);
+  console.log(distance(pops[2][1], pops[2][2], pops[7][1], pops[7][1])); // Atl to Mia
+  
+  console.log(pops[7][0], ' to ', pops[3][0]);
+  console.log(distance(pops[7][1], pops[7][2], pops[3][1], pops[3][1])); // Mia to DFW
+  
+  console.log(pops[3][0], ' to ', pops[4][0]);
+  console.log(distance(pops[3][1], pops[3][2], pops[4][1], pops[4][1])); // DFW to LAX
+  
+  console.log(pops[4][0], ' to ', pops[6][0]);
+  console.log(distance(pops[4][1], pops[4][2], pops[6][1], pops[6][1])); // LAX to Seattle
+  
+  console.log(pops[6][0], ' to ', pops[5][0]);
+  console.log(distance(pops[6][1], pops[6][2], pops[5][1], pops[5][1])); // Seattle to Chicago
+
+  console.log(pops[5][0], ' to ', pops[0][0]);
+  console.log(distance(pops[5][1], pops[5][2], pops[0][1], pops[0][1])); // Chicago to NYC
+
+  console.log(pops[2][0], ' to ', pops[3][0]);
+  console.log(distance(pops[2][1], pops[2][2], pops[3][1], pops[3][1])); // Atl to DFW
+
+};
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
