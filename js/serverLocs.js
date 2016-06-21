8bae4b8b928d0d8cade2233632ca9774e97fb06b
Kelly Rhodes <krhodes1@fullsail.edu>
2016-06-21 15:59:14 -0400

    begining of routing logic

--- js/serverLocs.js
+++ js/serverLocs.js
@@ -3,48 +3,56 @@ var serverLocs = [
     "id": 1,
     "name": "New York",
     "lat": 40.741355,
-    "lng": -74.003203
+    "lng": -74.003203,
+    'connections' : ["Washington DC", "Chicago"]
   },
   {
     "id": 2,
-    "name": "Ashburn",
+    "name": "Washington DC",
     "lat": 39.016363,
-    "lng": -77.459022
+    "lng": -77.459022,
+    'connections' : ["New York", "Atlanta"]
   },
   {
     "id": 3,
     "name": "Atlanta",
     "lat": 33.755456,
-    "lng": -84.39153
+    "lng": -84.39153,
+    'connections' : ["Washington DC", "Miami", "Dallas"]
   },
   {
     "id": 4,
     "name": "Dallas",
     "lat": 32.799852,
-    "lng": -96.820433
+    "lng": -96.820433,
+    'connections' : ["Atlanta", "Miami", "Los Angeles"]
   },
   {
     "id": 5,
     "name": "Los Angeles",
     "lat": 34.047908,
-    "lng": -118.255536
+    "lng": -118.255536,
+    'connections' : ["Dallas", "Seattle"]
   },
   {
     "id": 6,
     "name": "Chicago",
     "lat": 41.854159,
-    "lng": -87.619014
+    "lng": -87.619014,
+    'connections' : ["New York", "Seattle"]
   },
   {
     "id": 7,
     "name": "Seattle",
     "lat": 47.6143,
-    "lng": -122.3388
+    "lng": -122.3388,
+    'connections' : ["Los Angeles", "Chicago"]
   },
   {
     "id": 8,
     "name": "Miami",
     "lat": 25.782648,
-    "lng": -80.193157
+    "lng": -80.193157,
+    'connections' : ["Atlanta", "Dallas"]
   }
 ];
