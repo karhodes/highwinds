8b327b58c672c08edd98c16d45419e9fb8ac3787
Kelly Rhodes <krhodes1@fullsail.edu>
2016-06-24 07:40:47 -0400

    reverted gulpfile after eslint

--- gulpfile.js
+++ gulpfile.js
@@ -9,5 +9,5 @@ fs.readdirSync('./gulp').forEach(function (module) {
 // Calling the tasks built in the modules in the gulp folder
 gulp.task('build', ['js:build', 'css:build']);
 gulp.task('watch', ['js:watch', 'css:watch']);
-gulp.task('default', ['watch', 'lint']);
-//gulp.task('default', ['watch']);
+// gulp.task('default', ['watch', 'lint']);
+gulp.task('default', ['watch']);
